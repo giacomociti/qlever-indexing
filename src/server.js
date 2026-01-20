dc781902e9abb519c40b88c17858101959ca7680
Giacomo Citi <giacomo.citi@zazuko.com>
2026-01-20 11:41:14 +0100

    increase limit to 500mb

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 });
 
 // Middleware to parse raw body for POST /index
-app.use('/index', express.raw({ type: '*/*' }));
+app.use('/index', express.raw({ type: '*/*' , limit: '500mb' }));
 
 // command to create qlever index
 const getCommand = (configFile, inputFiles, outputFile) =>{
